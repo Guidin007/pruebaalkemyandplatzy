512608d93055259ab6b0e1b2d113651cf90c9e58
Guidin007 <guidorivasruiz@gmail.com>
2023-01-12 10:18:42 -0500

    se ajustan los Readme

--- Alkemy Real/limpieza_datos.py
+++
@@ -1,8 +0,0 @@
-
-import pandas as pd
-
-df=pd.read_csv(r"C:\Users\grman\BA1\pruebaalkemyandplatzy\Alkemy Real\Museo\2023-11\museos_datosabiertos-03-11-2021.csv", index_col="Code_Loc")
-# print("\nour CSV file= \n", pd)
-# museo.describe()
-df
-# museo_filtrado = museo.fillna (0)
