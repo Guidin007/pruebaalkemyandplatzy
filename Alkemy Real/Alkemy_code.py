c09a1ec52752b2b96b2d5834c03dae62ba7335d4
Guidin007 <guidorivasruiz@gmail.com>
2023-01-12 10:10:12 -0500

    Se limpian los archivos con Pandas de Python utilizando juperlab, tambien se documenta el codigo en readme

--- Alkemy Real/Alkemy_code.py
+++ Alkemy Real/Alkemy_code.py
@@ -1,22 +1,22 @@
 #Obtener los 3 archivos de fuente utilizando la librería requests y almacenarse en forma local (Ten en cuenta que las urls pueden cambiar en un futuro):
 
-import csv
-from os import lstat
+# import csv
+# from os import lstat
 
-import requests
-museo="https://datos.gob.ar/dataset/cultura-mapa-cultural-espacios-culturales/archivo/cultura_4207def0-2ff7-41d5-9095-d42ae8207a5d" 
-r = requests.get(museo)
+# import requests
+# museo="https://datos.gob.ar/dataset/cultura-mapa-cultural-espacios-culturales/archivo/cultura_4207def0-2ff7-41d5-9095-d42ae8207a5d" 
+# r = requests.get(museo)
 
-cine="https://datos.gob.ar/dataset/cultura-mapa-cultural-espacios-culturales/archivo/cultura_f7a8edb8-9208-41b0-8f19-d72811dcea97"
-r = requests.get(cine)
+# cine="https://datos.gob.ar/dataset/cultura-mapa-cultural-espacios-culturales/archivo/cultura_f7a8edb8-9208-41b0-8f19-d72811dcea97"
+# r = requests.get(cine)
 
-biblioteca= "https://datos.gob.ar/dataset/cultura-mapa-cultural-espacios-culturales/archivo/cultura_01c6c048-dbeb-44e0-8efa-6944f73715d7"
-r = requests.get(biblioteca)
+# biblioteca= "https://datos.gob.ar/dataset/cultura-mapa-cultural-espacios-culturales/archivo/cultura_01c6c048-dbeb-44e0-8efa-6944f73715d7"
+# r = requests.get(biblioteca)
 
-data = r.text
+# data = r.text
 
-print(data)
-
+# print(data)
+#------------------------------------------------------------------------------------------------
 # posible codigo para importar CVS desde la Url, esta pendiente 
 # import csv
 # import requests
@@ -44,7 +44,15 @@ print(data)
 # for linea in lista:
 #     print(linea)
 
+# Comando adecuado para leer los archivos csv. que se encuentran para el desarrollo del caso.
 import pandas as pd 
 
-biblioteca= pd.read_csv(r"C:\Users\grman\BA1\pruebaalkemyandplatzy\Alkemy Real\Biblioteca\2021-11\biblioteca_popular-03-11-2021.csv")
+museo= pd.read_csv(r"C:\Users\grman\BA1\pruebaalkemyandplatzy\Alkemy Real\Museo\2023-11\museos_limpios_datosabiertos-03-11-2021.csv")
+print("\nour CSV file= \n", museo)
+
+cine= pd.read_csv(r"C:\Users\grman\BA1\pruebaalkemyandplatzy\Alkemy Real\Cine\2021-11\salas_cine_limpio-03-11-2021.csv")
+print("\nour CSV file= \n", cine)
+
+biblioteca= pd.read_csv(r"C:\Users\grman\BA1\pruebaalkemyandplatzy\Alkemy Real\Biblioteca\2021-11\biblioteca_limpios_popular-03-11-2021.csv")
 print("\nour CSV file= \n", biblioteca)
+
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # pruebaalkemyandplatzy
 This is my first demo proyect  in Github
+# ----  Código utilizado en jupiterlab para la limpieza de datos, solo queda reemplazar el nombre del .csv y que este comando se encuentre en la misma carpeta donde se encuentran los archivos a limpiar para que no salga error.
+# import pandas as pd
+
+# df = pd.read_csv("salas_cine.csv", index_col="cod_localidad")
+
+# df
+
+#----------------------------------
+# Este comando es super importante te permite reemplazar todos las casillas en blanco que aparecen cono Nan a 0. nota: lo ejecutas en una nueva línea de comandos
+
+# df_filtrado = df.fillna (0)
+
+# df_filtrado.head ()
+
+#----------------------------------
+# Este te permite seleccionar los archivos que quieres visualizar en este caso los que tiene error en algo.
+# df. iloc[[19,49,50,51]]
+
+#----------------------------------
+# Este te permite seleccionar guardar los archivos en CSV una vez realizada la limpieza
+# df.to_csv("salas_cine_limpio-03-11-2021.csv")
